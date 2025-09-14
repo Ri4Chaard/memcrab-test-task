f123c211d277d553646070ec05d5aaf16da67448
Richard <yarik271202@gmail.com>
2025-09-14 18:12:51 +0300

    Header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Controls } from "./components/Controls/Controls";
+import { Header } from "./components/Header/Header";
 import { Table } from "./components/Table/Table";
 
 function App() {
   return (
     <div>
+      <Header />
       <Controls />
       <Table />
     </div>
